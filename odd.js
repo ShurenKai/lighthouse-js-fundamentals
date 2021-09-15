000e8ee326705385a8d7dae853d6f370a3f8b55a
Ren <cassandra.chen@uoit.net>
2021-09-15 15:57:01 +0000

    odd.js

---
+++ odd.js
@@ -0,0 +1,13 @@
+var number = '';
+function isOdd(num){
+  if (num % 2 === 0){
+    number = true
+  }
+  else{
+    number = false
+  }
+  return number
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
